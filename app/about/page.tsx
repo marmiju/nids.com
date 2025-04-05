4b821e3c98ededd8ea3a62cf372de9d66659d0f3
MAR miju <mar.miju.dev@gmail.com>
2025-04-05 16:23:21 +0600

    aboute initialize

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='max-w-[1200px] mx-auto '>
+            <div className='w-full color-orange h-96 grid justify-center items-center text-4xl rounded-2xl text-white font-semibold'>
+                <h1 className='text-center text-xl'>About Nids</h1>
+            </div>
+        </div>
+    )
+}
+export default page
