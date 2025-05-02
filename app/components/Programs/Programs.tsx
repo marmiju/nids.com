698d9235b3026c77b9e91738b6fb079f329d978b
mdnasimnirob <nasimnirob509668@gmail.com>
2025-05-02 16:17:25 +0600

    single program add

--- app/components/Programs/Programs.tsx
+++ app/components/Programs/Programs.tsx
@@ -1,5 +1,6 @@
 import { programs } from '@/lib/about/AboutData';
 import React from 'react';
+import Singleprogram from '../program/SingleProgram';
 
 type props = {
     programs:programs
@@ -8,13 +9,20 @@ type props = {
 const Programs:React.FC<props> = ({programs}) => {
     return (
         <div
-  style={{ width: `calc(100% - 200px)` }}
-  className="mx-auto mt-20   grid"
+ 
+  className="max-w-[1200px] mx-auto mt-20 space-y-6 p-[5%]  grid"
 >
 <div className=' w-full  grid justify-center '>
 <p className='text-center text-2xl md:text-3xl lg:text-4xl font-bold  '>{programs.title}</p>
 <p className='text-center' >{programs.desc}</p>
+</div >
+<div className='grid grid-cols-1 md:grid-cols-2 gap-4'>{
+    programs.program.map((data,index)=>{
+        return <Singleprogram key={index} program={data}></Singleprogram>
+    })
+}
 </div>
+
 </div>
     );
 };
